88c487d318675b77fd8b145947becbd9fc1db7e4
Bjoern Lange <bjoern.lange@duke.edu>
2015-09-09 19:05:22 -0400

    BL: Preparing read/write example, just layout, not functional yet

--- ELSI/src/elsi-interchange.f90
+++ ELSI/src/elsi-interchange.f90
@@ -8,36 +8,116 @@
 module ELSI
 
   use iso_c_binding
+  use HDF5
 
   implicit none
 
   PRIVATE ! By default, all routines contained are private
 
+  integer method = -1 !<Method for EV Solver (ELPA=1,OMM=2,PEXSI=3)
+  integer mode = -1 !<Mode for EV Solver (REAL_VALUES=1,COMPLEX_VALUES=2)
+
+  real*8, allocatable H_real, S_real 
+  complex*8, allocatable H_complex, S_complex
+  
   ! The following routines are public:
+
 
   public :: set_matrix_element
 
+  enum, bind( C )
+    enumerator :: ELPA, OMM, PEXSI
+  end enum
+
+  enum, bind( C )
+    enumerator :: REAL_VALUES, COMPLEX_VALUES
+  end enum   
+
+
 contains
 
-function set_matrix_element(i_row, i_col, element)
+subroutine set_method(i_method)
 
-!>
-!!  This routine sets a single element of the matrix
-!!
-!!  Parameters
-!!
-!!  i_row      row of the matrix
-!!
-!!  i_col      column of the matrix
-!!
-!!  element    value to be set
-!!
+   !>
+   !!  This routine sets the method of choice for solving the eigenvalue problem
+   !!
 
    implicit none
 
-   !! empty so far
+   integer, intent(in) :: i_method !<one of (ELPA,OMM,PEXSI)
+
+   method = i_method
+
+end subroutine set_method
+
+function allocate_matrices(n_rows, n_cols)
+
+!>
+!!  This routine prepares the matrices based on the dimension and method
+!!  specified
+
+   implicit none
+
+   integer, intent(in) :: n_rows !< Number of rows 
+   integer, intent(in) :: n_cols !< Number of cols
+   
+   select case (method)
+      case ELPA
+         select case (mode)
+            case COMPLEX_VALUES
+               allocate(H_complex (n_rows, n_cols))      
+               allocate(S_complex (n_rows, n_cols))
+            case REAL_VALUES
+               allocate(H_real (n_rows, n_cols))      
+               allocate(S_real (n_rows, n_cols))
+         end select
+      case OMM
+         write(*,'a') "OMM not implemented yet!"
+         stop
+      case PEXSI
+         write(*,'a') "PEXSI not implemented yet!"
+         stop
+      case DEFAULT
+         write(*,'2a') "No method has been chosen. " &
+            "Please choose method ELPA, OMM, or PEXSI"
+         stop
+      end select
 
    return 0
 end function
+
+function deallocate_matrices()
+
+!>
+!!  This routine prepares the matrices based on the dimension and method
+!!  specified
+
+   implicit none
+
+   select case (method)
+      case ELPA
+         select case (mode)
+            case COMPLEX_VALUES
+               deallocate(H_complex)      
+               deallocate(S_complex)
+            case REAL_VALUES
+               deallocate(H_real)      
+               deallocate(S_real)
+         end select
+      case OMM
+         write(*,'a') "OMM not implemented yet!"
+         stop
+      case PEXSI
+         write(*,'a') "PEXSI not implemented yet!"
+         stop
+      case DEFAULT
+         write(*,'2a') "No method has been chosen. " &
+            "Please choose method ELPA, OMM, or PEXSI"
+         stop
+      end select
+
+   return 0
+end function
+
 
 end module ELSI
---
+++ ELSI/src/readwrite.f90
@@ -0,0 +1,161 @@
+!> ELSI Interchange
+!! 
+!! Copyright of the original code rests with the authors inside the ELSI
+!! consortium. The copyright of any additional modifications shall rest
+!! with their original authors, but shall adhere to the licensing terms
+!! distributed along with the original code in the file "COPYING".
+
+subroutine test
+
+  use iso_c_binding
+  use ELSI
+  use ELPA
+
+  implicit none
+  include 'mpi.h'
+
+  ! This is the ELSI test suite
+  ! First we will test the writing and reading of a matrix to a file
+
+  ! Ok, let us first create a matrix for the ELSI interface
+  ! Something like
+
+  integer, parameter :: n_dim = 100
+
+  ! Coarse grid
+  integer np_rows        !< Number of rows (coarse grid)
+  integer np_cols        !< Number of cols (coarse grid)
+  
+  ! Sub grid
+  integer n_rows        !< Total number of rows for this process
+  integer n_cols        !< Total number of columns for this process
+
+  ! MPI variables
+  integer myid           !< local process id
+  integer mpierr         !< mpi error handler
+  integer mpi_comm_world !< global mpi communicator
+  integer n_procs        !< number of mpi processes
+
+  ! BLACS variable
+  integer blacs_ctxt     !< local blacs context
+  integer mpi_comm_rows  !< row communicatior
+  integer mpi_comm_cols  !< row communicatior
+  integer ip_row         !< local row    position
+  integer ip_col         !< local column position
+
+  ! Random number generator
+  integer :: iseed(4096) !< Random seed
+
+  ! Test matrices
+  real*8, allocatable :: h(:,:)
+  real*8, allocatable :: s(:,:)
+  real*8, allocatable :: buffer(:,:) 
+
+  ! MPI Initialization
+
+  call mpi_init(mpierr)
+  call mpi_comm_rank(mpi_comm_world, myid, mpierr)
+  call mpi_comm_size(mpi_comm_world, n_procs, mpierr)
+ 
+  ! Define blockcyclic setup
+  do np_cols = NINT(SQRT(REAL(nprocs))),2,-1
+     if(mod(nprocs,np_cols) == 0 ) exit
+  enddo
+
+  np_rows = n_procs / np_cols
+
+  ! Set up BLACS and MPI communicators
+
+  my_blacs_ctxt = mpi_comm_world
+  call BLACS_Gridinit( my_blacs_ctxt, 'C', np_rows, np_cols )
+  call BLACS_Gridinfo( my_blacs_ctxt, np_rows, np_cols, ip_prow, ip_col )
+
+  call descinit( sc_desc, n_dim, n_dim, 16, 16, 0, 0, &
+                 my_blacs_ctxt, n_rows, info )
+
+  call get_elpa_row_col_comms( mpi_comm_world, ip_row, ip_col, &
+                               mpi_comm_rows, mpi_comm_cols)
+
+  ! Generate some data
+  allocate(h(n_dim,n_dim))
+  allocate(s(n_dim,n_dim))
+
+  iseed(:) = myid
+  call RANDOM_SEED(put=iseed)
+  call RANDOM_NUMBER(buffer)
+
+  ! Symmetrize hamiltonian
+  h = buffer
+  ! h = h + buffer**T
+  call pdtran( n_dim, n_dim, 1.d0, buffer, 1, 1, sc_desc, &
+               1.d0, h, 1, 1, sc_desc)
+
+  call RANDOM_NUMBER(buffer)
+  s = buffer
+  ! s = s + buffer**T
+  call pdtran( n_dim, n_dim, 1.d0, buffer, 1, 1, sc_desc, &
+               1.d0, s, 1, 1, sc_desc)
+
+
+  ! Now set some ELSI specifications
+  call elsi_set_method(ELPA)
+  call elsi_set_mode(REAL_VALUES)
+
+  ! Initialize the data space
+  call elsi_allocate_matrices(n_rows,n_cols)
+
+  ! Set matrices
+  call elsi_set_hamiltonian(h)
+  call elsi_set_overlap(s)
+
+  ! Write eigenvalue problem
+  call elsi_write_ev_problem("elsi_eigenvalue_problem.hdf5")
+
+  ! Read eigenvalue problem 
+  call elsi_read_ev_problem("elsi_eigenvalue_problem.hdf5")
+
+  call elsi_get_hamiltonian(buffer)
+  buffer = buffer - h
+
+  errmax = 0
+  do i=1,n_cols
+      colerr = 0
+      call pdnrm2(na,colerr,buffer,1,i,sc_desc,1)
+      errmax = max(errmax, colerr)
+  enddo
+
+  ! Get maximum error norm over all processors
+  colerr = errmax
+  call mpi_allreduce(colerr,errmax,1,MPI_REAL8,MPI_MAX,MPI_COMM_WORLD,mpierr)
+  if(myid==0) print *
+  if(myid==0) print *,'Error Residual for H    :',errmax
+
+  call elsi_get_overlap(buffer)
+  buffer = buffer - s
+
+  errmax = 0
+  do i=1,n_cols
+      colerr = 0
+      call pdnrm2(na,colerr,buffer,1,i,sc_desc,1)
+      errmax = max(errmax, colerr)
+  enddo
+
+  ! Get maximum error norm over all processors
+  colerr = errmax
+  call mpi_allreduce(colerr,errmax,1,MPI_REAL8,MPI_MAX,MPI_COMM_WORLD,mpierr)
+  if(myid==0) print *
+  if(myid==0) print *,'Error Residual for S    :',errmax
+
+
+  ! Deallocations
+  deallocate(h) 
+  deallocate(s) 
+
+
+  ! exit BLACS grid
+  call blacs_gridexit(my_blacs_ctxt)
+
+  ! finalize mpi
+  call mpi_finalize(mpierr)
+
+end subroutine test
