9754ccacd3c7cf31d3f943dbd65257356154a3d7
Bjoern Lange <bjoern.lange@duke.edu>
2015-10-05 20:29:55 -0400

    BL: mv examples to more descriptive example folder; prepare demonstrator placeholders for OMM (not working yet!!!)

---
+++ ELSI/example/read_and_solve_omm.f90
@@ -0,0 +1,92 @@
+!Copyright (c) 2015, ELSI consortium
+!All rights reserved.
+!
+!Redistribution and use in source and binary forms, with or without
+!modification, are permitted provided that the following conditions are met:
+! * Redistributions of source code must retain the above copyright
+!   notice, this list of conditions and the following disclaimer.
+! * Redistributions in binary form must reproduce the above copyright
+!   notice, this list of conditions and the following disclaimer in the
+!   documentation and/or other materials provided with the distribution.
+! * Neither the name of the ELSI project nor the
+!   names of its contributors may be used to endorse or promote products
+!   derived from this software without specific prior written permission.
+!
+!THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+!AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+!IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+!DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+!FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+!DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+!SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+!CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+!OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+!OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+!> 
+!! This program illustratrates how to read and solve an eigenvalue problem 
+!! created by the ELSI interface
+!!
+
+program read_and_solve
+
+  use iso_c_binding
+  use ELSI
+  use ELSI_MPI_TOOLS
+
+  implicit none
+  include 'mpif.h'
+
+  ! This is the ELSI test suite
+  ! First we will test the writing and reading of a matrix to a file
+
+  integer :: n_eigenvectors = 10
+
+  integer :: myid, i_eigenvector
+  real*8, allocatable :: eigenvals(:)
+  integer :: matrixsize, block_rows, block_cols
+
+
+  !  Pharse command line argumnents, if given
+   INTEGER*4 :: iargc
+   character*16 arg1
+
+   if (iargc() == 1) then
+      call getarg(1, arg1)
+      read(arg1, *) n_eigenvectors
+   endif
+
+
+  ! Now set some ELSI specifications
+  call elsi_initialize_mpi()
+  call elsi_initialize_problem_from_file("elsi_eigenvalue_problem.hdf5")
+  call elsi_initialize_blacs()
+  call elsi_set_method(OMM_DENSE)
+  call elsi_set_mode(REAL_VALUES)
+
+  ! Read eigenvalue problem
+  call elsi_allocate_matrices()
+  call elsi_read_ev_problem("elsi_eigenvalue_problem.hdf5")
+
+  call elsi_get_global_dimensions(matrixsize,block_rows,block_cols)
+  allocate (eigenvals(n_eigenvectors))
+
+  ! Solve the eigenvalue problem
+  call elsi_solve_ev_problem(n_eigenvectors)
+  
+  call elsi_get_eigenvalues(eigenvals, n_eigenvectors)
+
+  call elsi_get_myid(myid)
+  if (myid == 0) then 
+     do i_eigenvector = 1,n_eigenvectors
+        write (*,'(I5,A,E13.6)'), i_eigenvector," Eigenvalue : ", &
+           eigenvals(i_eigenvector)
+     end do
+  end if
+
+  deallocate(eigenvals)
+
+  ! elsi shutdown
+  call elsi_finalize()
+
+end program
--- ELSI/regression/regression.sh
+++ ELSI/regression/regression.sh
@@ -2,50 +2,50 @@
 
 # Construct and Write
 cd construct_and_write
-mpirun -n 4 ../../bin/construct_and_write 100 10 > /dev/null
+mpirun -n 4 ../../bin/construct_and_write_elpa 100 10 > /dev/null
 fail=`h5diff elsi_eigenvalue_problem.hdf5 data/elsi_eigenvalue_problem.hdf5`
 if [ "x$fail" != "x" ]; then
-   echo "construct_and_write failed"
+   echo "construct_and_write_elpa failed"
 else
-   echo "construct_and_write successfull"
+   echo "construct_and_write_elpa successfull"
    rm elsi_eigenvalue_problem.hdf5
 fi
 cd ..
 
 # Set and Write
 cd set_and_write
-mpirun -n 4 ../../bin/set_and_write 100 10 > /dev/null
+mpirun -n 4 ../../bin/set_and_write_elpa 100 10 > /dev/null
 fail=`h5diff elsi_eigenvalue_problem.hdf5 data/elsi_eigenvalue_problem.hdf5`
 if [ "x$fail" != "x" ]; then
-   echo "set_and_write failed"
+   echo "set_and_write_elpa failed"
 else
-   echo "set_and_write successfull"
+   echo "set_and_write_elpa successfull"
    rm elsi_eigenvalue_problem.hdf5
 fi
 cd ..
 
 # Read and Write
 cd read_and_write
-mpirun -n 4 ../../bin/read_and_write > /dev/null
+mpirun -n 4 ../../bin/read_and_write_elpa > /dev/null
 fail=`h5diff elsi_eigenvalue_problem_out.hdf5 data/elsi_eigenvalue_problem_out.hdf5`
 if [ "x$fail" != "x" ]; then
-   echo "read_and_write failed"
+   echo "read_and_write_elpa failed"
 else
-   echo "read_and_write successfull"
+   echo "read_and_write_elpa successfull"
    rm elsi_eigenvalue_problem_out.hdf5
 fi
 cd ..
 
 # Read and Solve
 cd read_and_solve
-mpirun -n 4 ../../bin/read_and_solve 100 > buffer.dat
+mpirun -n 4 ../../bin/read_and_solve_elpa 100 > buffer.dat
 tail -n 100 buffer.dat > eigenvalues.dat
 rm buffer.dat
 fail=`diff eigenvalues.dat data/eigenvalues.dat`
 if [ "x$fail" != "x" ]; then
-   echo "read_and_solve failed"
+   echo "read_and_solve_elpa failed"
 else
-   echo "read_and_solve successfull"
+   echo "read_and_solve_elpa successfull"
    rm eigenvalues.dat
 fi
 cd ..
