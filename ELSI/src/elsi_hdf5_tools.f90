458fb19290990ee68f87ab2cfbdd672bf986dae3
Bjoern Lange <bjoern.lange@duke.edu>
2015-11-16 15:06:17 -0500

    BL: Introduce elsi_stop; Set chunks for matrix read/write improves writing process by factor ten on BluegeneQ

--- ELSI/src/elsi_hdf5_tools.f90
+++ ELSI/src/elsi_hdf5_tools.f90
@@ -45,7 +45,11 @@ module ELSI_HDF5_TOOLS
   integer(HSIZE_T) :: offset(2) !< Matrix offset 
   integer(HSIZE_T) :: stride(2) !< Distance between blocks 
   integer(HSIZE_T) :: block(2)  !< Dimension of block
+  integer(HSIZE_T) :: chunk(2)  !< Dimension of chunks
   logical :: incomplete         !< Is block description complete?
+
+  integer(HSIZE_T) :: nslots = 9973 !< Number of Slots for chunk hash table
+  integer(HSIZE_T) :: nchunk_bytes = 2*1024*1024 !< Size of chunk cache
 
 
   interface hdf5_write_attribute
@@ -86,11 +90,12 @@ module ELSI_HDF5_TOOLS
 subroutine hdf5_initialize()
    
    implicit none
+   
+   character*40 :: caller = "hdf5_initialize"
 
    call H5open_f( h5err )
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to initialize Fortran interface."
-      stop
+      call elsi_stop("HDF5: Failed to initialize Fortran interface.",caller)
    end if
 
 end subroutine
@@ -102,10 +107,11 @@ subroutine hdf5_finalize()
    
    implicit none
 
+   character*40 :: caller = "hdf5_finalize"
+
    call H5close_f( h5err )
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to finalize Fortran interface."
-      stop
+      call elsi_stop("HDF5: Failed to finalize Fortran interface.",caller)
    end if
 
 end subroutine
@@ -122,26 +128,26 @@ subroutine hdf5_create_file(file_name, mpi_comm_global, &
    integer, intent(in) :: mpi_comm_global
    integer, intent(in) :: mpi_info_null
    integer, intent(out) :: file_id
+   character*40 :: caller = "hdf5_create_file"
 
    integer(hid_t) :: plist_id
 
    call H5Pcreate_f( H5P_FILE_ACCESS_F, plist_id, h5err )
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to create property list for parallel access."
-      stop
+      call elsi_stop("HDF5: Failed to create parallel property list.", &
+            caller)
    end if
 
    call H5Pset_fapl_mpio_f(plist_id, mpi_comm_global, mpi_info_null, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to set property list for parallel access."
-      stop
+      call elsi_stop("HDF5: Failed to set MPI-IO for parallel access.", &
+            caller)
    end if
 
    call H5Fcreate_f( file_name, H5F_ACC_TRUNC_F, file_id, h5err, & 
                      access_prp = plist_id)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to create file for parallel access."
-      stop
+      call elsi_stop("HDF5: Failed to create file for parallel access.", caller)
    end if
 
 end subroutine
@@ -154,29 +160,29 @@ subroutine hdf5_open_file(file_name, mpi_comm_global, &
   
    implicit none
    character(len=*), intent(in) :: file_name
-   integer, intent(in) :: mpi_comm_global
-   integer, intent(in) :: mpi_info_null
+   integer, intent(in)  :: mpi_comm_global
+   integer, intent(in)  :: mpi_info_null
    integer, intent(out) :: file_id
+   character*40         :: caller = "hdf5_open_file"
 
    integer(hid_t) :: plist_id
 
    call H5Pcreate_f( H5P_FILE_ACCESS_F, plist_id, h5err )
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to create property list for parallel access."
-      stop
+      call elsi_stop("HDF5: Failed to create parallel property list.",&
+            caller)
    end if
 
    call H5Pset_fapl_mpio_f(plist_id, mpi_comm_global, mpi_info_null, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to set property list for parallel access."
-      stop
+      call elsi_stop("HDF5: Failed to set MPI-IO for parallel access.",&
+            caller)
    end if
 
    call H5Fopen_f( file_name, H5F_ACC_RDONLY_F, file_id, h5err, & 
                      access_prp = plist_id)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to create file for parallel access."
-      stop
+      call elsi_stop("HDF5: Failed to open file for parallel access.",caller)
    end if
 
 end subroutine
@@ -190,10 +196,11 @@ subroutine hdf5_close_file(file_id)
    implicit none
    integer, intent(in) :: file_id
 
+   character*40 :: caller = "hdf5_close_file"
+
    call H5Fclose_f( file_id, h5err )
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to close file for parallel access."
-      stop
+      call elsi_stop ("HDF5: Failed to close file for parallel access.", caller)
    end if
 
 
@@ -210,10 +217,11 @@ subroutine hdf5_create_group (place_id, group_name, group_id)
    character(len=*), intent(in) :: group_name
    integer(hid_t), intent(out)  :: group_id
 
+   character*40 :: caller = "hdf5_create_group"
+
    call H5Gcreate_f( place_id, group_name, group_id, h5err)
    if (h5err /= 0) then
-      write(*,'(a,a)') "HDF5: Failed to create group ", group_name
-      stop
+      call elsi_stop("HDF5: Failed to create group " // group_name, caller)
    end if
  
 end subroutine
@@ -229,10 +237,11 @@ subroutine hdf5_open_group (place_id, group_name, group_id)
    character(len=*), intent(in) :: group_name
    integer(hid_t), intent(out)  :: group_id
 
+   character*40 :: caller = "hdf5_open_group"
+
    call H5Gopen_f( place_id, group_name, group_id, h5err)
    if (h5err /= 0) then
-      write(*,'(a,a)') "HDF5: Failed to create group ", group_name
-      stop
+      call elsi_stop("HDF5: Failed to create group " // group_name, caller)
    end if
 
 end subroutine
@@ -247,10 +256,11 @@ subroutine hdf5_close_group (group_id)
 
    integer(hid_t), intent(in)  :: group_id
 
+   character*40 :: caller = "hdf5_close_group"
+
    call H5Gclose_f( group_id, h5err)
    if (h5err /= 0) then
-      write(*,'(a,a)') "HDF5: Failed to close group."
-      stop
+      call elsi_stop("HDF5: Failed to close group.", caller)
    end if
 
 end subroutine
@@ -266,41 +276,37 @@ subroutine hdf5_write_attribute_integer(place_id, attr_name, attr_value)
    character(len=*), intent(in) :: attr_name
    integer, intent(in)          :: attr_value
 
-   integer(hid_t) :: space_id
-   integer(hid_t) :: attr_id
+   integer(hid_t)   :: space_id
+   integer(hid_t)   :: attr_id
    integer(hsize_t) :: dims(1)
 
+   character*40     :: caller = "hdf5_write_attribute_integer"
 
    call H5Screate_f( H5S_SCALAR_F, space_id, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to create Scalar Space Identifier."
-      stop
+      call elsi_stop("HDF5: Failed to create Scalar Space Identifier.",caller)
    end if
 
    call H5Acreate_f( place_id, attr_name, H5T_NATIVE_INTEGER, space_id, &
                      attr_id, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to create Attribute Identifier."
-      stop
+      call elsi_stop("HDF5: Failed to create Attribute Identifier.",caller)
    end if
 
    dims = 1
    call H5Awrite_f ( attr_id, H5T_NATIVE_INTEGER, attr_value, dims, h5err )
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to write to Attribute Identifier."
-      stop
+      call elsi_stop("HDF5: Failed to write to Attribute Identifier.",caller)
    end if
 
    call H5Aclose_f ( attr_id, h5err )
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to close to Attribute Identifier."
-      stop
+      call elsi_stop("HDF5: Failed to close to Attribute Identifier.",caller)
    end if
 
    call H5Sclose_f ( space_id, h5err )
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to close to Space Identifier."
-      stop
+      call elsi_stop("HDF5: Failed to close to Space Identifier.",caller)
    end if
 
 end subroutine
@@ -357,6 +363,7 @@ subroutine hdf5_write_matrix_parallel_double(place_id, matrix_name, matrix)
    integer(hid_t) :: memspace
    integer(hid_t) :: filespace
    integer(hid_t) :: data_id
+   integer(hid_t) :: dataprop_id
    integer(hid_t) :: plist_id
 
    integer(HSIZE_T) :: add_count(2) !< Number of blocks 
@@ -364,33 +371,46 @@ subroutine hdf5_write_matrix_parallel_double(place_id, matrix_name, matrix)
    integer(HSIZE_T) :: miss_count(2) !< Number of blocks 
    integer(HSIZE_T) :: miss_offset(2) !< Matrix offset
    integer          :: i_block 
+   character*40     :: caller = "hdf5_write_matrix_parallel_double"
 
 
    call H5Screate_simple_f (2, local_dim, memspace, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to create local Matrix Space."
-      stop
+      call elsi_stop("HDF5: Failed to create local Matrix Space.",caller)
    end if
 
    call H5Screate_simple_f (2, global_dim, filespace, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to create global Matrix Space."
-      stop
+      call elsi_stop("HDF5: Failed to create global Matrix Space.",caller)
    end if
 
+   ! Create dataset chunks according to blocksize
+   call H5Pcreate_f(H5P_DATASET_CREATE_F, dataprop_id, h5err)
+   if (h5err /= 0) then
+      call elsi_stop("HDF5: Failed to create dataset property list.",caller)
+   end if
+
+   ! Set cache to 5 MB and slots to a prime number 
+   call H5Pset_chunk_cache_f(dataprop_id,nslots,nchunk_bytes,1.0,h5err)
+   if (h5err /= 0) then
+      call elsi_stop("HDF5: Failed to create dataset chunck cache.",caller)
+   end if
+
+   call H5Pset_chunk_f(dataprop_id,2,chunk,h5err)
+   if (h5err /= 0) then
+      call elsi_stop("HDF5: Failed to create dataset chunks property.",caller)
+   end if
 
    call H5Dcreate_f (place_id, matrix_name, H5T_NATIVE_REAL, filespace,&
-                   data_id, h5err) 
+                   data_id, h5err, dataprop_id) 
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to create Matrix Data Identifier."
-      stop
+      call elsi_stop("HDF5: Failed to create Matrix Data Identifier.",caller)
    end if
 
    call H5Sselect_hyperslab_f (filespace, H5S_SELECT_SET_F, offset, count,&
          h5err, stride, block)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to write select Hyperslap."
-      stop
+      call elsi_stop("HDF5: Failed to write select Hyperslap.", caller)
    end if
 
    if (incomplete) then
@@ -405,8 +425,7 @@ subroutine hdf5_write_matrix_parallel_double(place_id, matrix_name, matrix)
             call H5Sselect_hyperslab_f (filespace, H5S_SELECT_OR_F, add_offset,&
                add_count, h5err)
             if (h5err /= 0) then
-               write(*,'(a)') "HDF5: Failed to write select Hyperslap."
-               stop
+               call elsi_stop("HDF5: Failed to write select Hyperslap.",caller)
             end if
          end do
       end if
@@ -419,8 +438,7 @@ subroutine hdf5_write_matrix_parallel_double(place_id, matrix_name, matrix)
             call H5Sselect_hyperslab_f (filespace, H5S_SELECT_OR_F, add_offset,&
                add_count, h5err)
             if (h5err /= 0) then
-               write(*,'(a)') "HDF5: Failed to write select Hyperslap."
-               stop
+               call elsi_stop("HDF5: Failed to write select Hyperslap.",caller)
             end if
          end do
       end if
@@ -430,44 +448,53 @@ subroutine hdf5_write_matrix_parallel_double(place_id, matrix_name, matrix)
          call H5Sselect_hyperslab_f (filespace, H5S_SELECT_OR_F, miss_offset,&
                miss_count, h5err)
          if (h5err /= 0) then
-            write(*,'(a)') "HDF5: Failed to write select Hyperslap."
-            stop
+            call elsi_stop("HDF5: Failed to write select Hyperslap.",caller)
          end if
       end if
 
    end if
 
-   CALL h5pcreate_f(H5P_DATASET_XFER_F, plist_id, h5err) 
-   CALL h5pset_dxpl_mpio_f(plist_id, H5FD_MPIO_COLLECTIVE_F, h5err)
+   call h5pcreate_f(H5P_DATASET_XFER_F, plist_id, h5err)
+   if (h5err /= 0) then
+      call elsi_stop("HDF5: Failed to create dataset property list.",caller)
+   end if
+ 
+   call h5pset_dxpl_mpio_f(plist_id, H5FD_MPIO_COLLECTIVE_F, h5err)
+   if (h5err /= 0) then
+      call elsi_stop("HDF5: Failed to MPI-IO.",caller)
+   end if
+
 
    call H5Dwrite_f (data_id, H5T_NATIVE_DOUBLE, matrix, &
          local_dim, h5err, mem_space_id = memspace, file_space_id = filespace, &
          xfer_prp = plist_id)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to write Matrix to Filespace."
-      stop
+      call elsi_stop("HDF5: Failed to write Matrix to Filespace.",caller)
    end if
 
 
    call H5Sclose_f(filespace, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to close Filespace."
-      stop
+      call elsi_stop("HDF5: Failed to close Filespace.",caller)
    end if
 
 
    call H5Sclose_f(memspace, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to close Memspace."
-      stop
+      call elsi_stop("HDF5: Failed to close Memspace.",caller)
    end if
 
 
    call H5Dclose_f(data_id, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to close Data Identifier."
-      stop
+      call elsi_stop("HDF5: Failed to close Data Identifier.",caller)
    end if
+
+   call H5Pclose_f(dataprop_id, h5err)
+   if (h5err /= 0) then
+      call elsi_stop("HDF5: Failed to close Data Identifier.",caller)
+   end if
+
 
 
 end subroutine
@@ -486,39 +513,52 @@ subroutine hdf5_read_matrix_parallel_double(place_id, matrix_name, matrix)
    integer(hid_t) :: memspace
    integer(hid_t) :: filespace
    integer(hid_t) :: data_id
+   integer(hid_t) :: dataprop_id
    integer(hid_t) :: plist_id
 
    integer(HSIZE_T) :: add_count(2) !< Number of blocks 
    integer(HSIZE_T) :: add_offset(2) !< Matrix offset
    integer(HSIZE_T) :: miss_count(2) !< Number of blocks 
    integer(HSIZE_T) :: miss_offset(2) !< Matrix offset
-   integer          :: i_block 
+   integer          :: i_block
+   character*40     :: caller = "hdf5_read_matrix_parallel_double" 
  
 
    call H5Screate_simple_f (2, local_dim, memspace, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to create local Matrix Space."
-      stop
+      call elsi_stop("HDF5: Failed to create local Matrix Space.",caller)
    end if
 
    call H5Screate_simple_f (2, global_dim, filespace, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to create global Matrix Space."
-      stop
+      call elsi_stop("HDF5: Failed to create global Matrix Space.",caller)
    end if
 
-
-   call H5Dopen_f (place_id, matrix_name, data_id, h5err) 
+   ! Create dataset chunks according to blocksize
+   call H5Pcreate_f(H5P_DATASET_ACCESS_F, dataprop_id, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to create Matrix Data Identifier."
-      stop
+      call elsi_stop("HDF5: Failed to create dataset property list.",caller)
+   end if
+
+   call H5Pset_chunk_cache_f(dataprop_id,nslots,nchunk_bytes,1.0,h5err)
+   if (h5err /= 0) then
+      call elsi_stop("HDF5: Failed to create dataset chunck cache.",caller)
+   end if
+
+   call H5Pset_chunk_f(dataprop_id,2,chunk,h5err)
+   if (h5err /= 0) then
+      call elsi_stop("HDF5: Failed to create dtaset chunks property.",caller)
+   end if
+
+   call H5Dopen_f (place_id, matrix_name, data_id, h5err, dataprop_id) 
+   if (h5err /= 0) then
+      call elsi_stop("HDF5: Failed to create Matrix Data Identifier.", caller)
    end if
 
    call H5Sselect_hyperslab_f (filespace, H5S_SELECT_SET_F, offset, count,&
          h5err, stride, block)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to write select Hyperslap."
-      stop
+      call elsi_stop("HDF5: Failed to write select Hyperslap.",caller)
    end if
 
    if (incomplete) then
@@ -533,8 +573,7 @@ subroutine hdf5_read_matrix_parallel_double(place_id, matrix_name, matrix)
             call H5Sselect_hyperslab_f (filespace, H5S_SELECT_OR_F, add_offset,&
                add_count, h5err)
             if (h5err /= 0) then
-               write(*,'(a)') "HDF5: Failed to write select Hyperslap."
-               stop
+               call elsi_stop("HDF5: Failed to write select Hyperslap.",caller)
             end if
          end do
       end if
@@ -547,8 +586,7 @@ subroutine hdf5_read_matrix_parallel_double(place_id, matrix_name, matrix)
             call H5Sselect_hyperslab_f (filespace, H5S_SELECT_OR_F, add_offset,&
                add_count, h5err)
             if (h5err /= 0) then
-               write(*,'(a)') "HDF5: Failed to write select Hyperslap."
-               stop
+               call elsi_stop("HDF5: Failed to write select Hyperslap.",caller)
             end if
          end do
       end if
@@ -558,8 +596,7 @@ subroutine hdf5_read_matrix_parallel_double(place_id, matrix_name, matrix)
          call H5Sselect_hyperslab_f (filespace, H5S_SELECT_OR_F, miss_offset,&
                miss_count, h5err)
          if (h5err /= 0) then
-            write(*,'(a)') "HDF5: Failed to write select Hyperslap."
-            stop
+            call elsi_stop("HDF5: Failed to write select Hyperslap.",caller)
          end if
       end if
 
@@ -573,31 +610,31 @@ subroutine hdf5_read_matrix_parallel_double(place_id, matrix_name, matrix)
          local_dim, h5err, mem_space_id = memspace, file_space_id = filespace, &
          xfer_prp = plist_id)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to write Matrix to Filespace."
-      stop
+      call elsi_stop("HDF5: Failed to write Matrix to Filespace.",caller)
    end if
 
 
    call H5Sclose_f(filespace, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to close Filespace."
-      stop
+      call elsi_stop("HDF5: Failed to close Filespace.",caller)
    end if
 
 
    call H5Sclose_f(memspace, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to close Memspace."
-      stop
+      call elsi_stop("HDF5: Failed to close Memspace.",caller)
    end if
 
 
    call H5Dclose_f(data_id, h5err)
    if (h5err /= 0) then
-      write(*,'(a)') "HDF5: Failed to close Data Identifier."
-      stop
+      call elsi_stop("HDF5: Failed to close Data Identifier.",caller)
    end if
 
+   call H5Pclose_f(dataprop_id, h5err)
+   if (h5err /= 0) then
+      call elsi_stop("HDF5: Failed to close Data Identifier.",caller)
+   end if
 
 end subroutine
 
@@ -636,6 +673,8 @@ subroutine hdf5_get_scalapack_pattern()
         incomplete = .False.
       end if
 
+      chunk = block
+
    else
 
       offset = process_position * block
@@ -650,6 +689,8 @@ subroutine hdf5_get_scalapack_pattern()
       else
         incomplete = .False.
       end if
+
+      chunk = 10 * block
 
    end if
 
