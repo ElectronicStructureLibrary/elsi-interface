5651596cad14879104f9a651cbf79e5ff835cae1
Victor Yu <wy29@duke.edu>
2017-09-02 21:19:18 -0700

    VY: Added sparse test cases to "make check".

--- src/ELSI/make_checkf.sh
+++ src/ELSI/make_checkf.sh
@@ -2,15 +2,23 @@
 # VY: This is a simple bash script used for "make checkf"
 
 rm ev_real_serial.log 2> /dev/null
-rm ev_real_elpa.log 2> /dev/null
-rm dm_real_elpa.log 2> /dev/null
-rm dm_real_libomm.log libOMM.log 2> /dev/null
-rm dm_real_pexsi.log logPEXSI0 2> /dev/null
 rm ev_complex_serial.log 2> /dev/null
+rm ev_real_elpa.log 2> /dev/null
 rm ev_complex_elpa.log 2> /dev/null
+rm ev_real_sparse_elpa.log 2> /dev/null
+#rm ev_complex_sparse_elpa.log 2> /dev/null
+rm dm_real_elpa.log 2> /dev/null
 rm dm_complex_elpa.log 2> /dev/null
+rm dm_real_sparse_elpa.log 2> /dev/null
+#rm dm_complex_sparse_elpa.log 2> /dev/null
+rm dm_real_libomm.log libOMM.log 2> /dev/null
 rm dm_complex_libomm.log libOMM.log 2> /dev/null
+rm dm_real_sparse_libomm.log 2> /dev/null
+#rm dm_complex_sparse_libomm.log 2> /dev/null
+rm dm_real_pexsi.log logPEXSI0 2> /dev/null
 rm dm_complex_pexsi.log logPEXSI0 2> /dev/null
+rm dm_real_sparse_pexsi.log 2> /dev/null
+#rm dm_complex_sparse_pexsi.log 2> /dev/null
 set -e # Stop on error
 
 RED_ALART="false"
@@ -102,6 +110,27 @@ else
 fi
 
 echo
+echo -n "Running the parallel 'elsi_ev_real_sparse + ELPA' Fortran test"
+${MPI_EXEC} -n 4 ./test_ev_real_sparse.x 1 ${ELSI_DIR}/test/H_real.csc ${ELSI_DIR}/test/S_real.csc 1 > ev_real_sparse_elpa.log &
+PID=$!
+while kill -0 $PID 2>/dev/null; do
+    sleep 1
+    echo -n '.'
+done
+
+if (! grep -q "Passed" <./ev_real_sparse_elpa.log); then
+   tput setaf 5
+   RED_ALART="true"
+   echo " FAILED!"
+   tput sgr0
+   echo "See `pwd`/ev_real_sparse_elpa.log for details."
+else
+   tput setaf 10
+   echo " PASSED!"
+   tput sgr0
+fi
+
+echo
 echo -n "Running the parallel 'elsi_dm_real + ELPA' Fortran test"
 ${MPI_EXEC} -n 4 ./test_dm_real.x 1 ${ELSI_DIR}/test/H_real.csc ${ELSI_DIR}/test/S_real.csc 1 > dm_real_elpa.log &
 PID=$!
@@ -137,6 +166,27 @@ if (! grep -q "Passed" <./dm_complex_elpa.log); then
    echo " FAILED!"
    tput sgr0
    echo "See `pwd`/dm_complex_elpa.log for details."
+else
+   tput setaf 10
+   echo " PASSED!"
+   tput sgr0
+fi
+
+echo
+echo -n "Running the parallel 'elsi_dm_real_sparse + ELPA' Fortran test"
+${MPI_EXEC} -n 4 ./test_dm_real_sparse.x 1 ${ELSI_DIR}/test/H_real.csc ${ELSI_DIR}/test/S_real.csc 1 > dm_real_sparse_elpa.log &
+PID=$!
+while kill -0 $PID 2>/dev/null; do
+    sleep 1
+    echo -n '.'
+done
+
+if (! grep -q "Passed" <./dm_real_sparse_elpa.log); then
+   tput setaf 5
+   RED_ALART="true"
+   echo " FAILED!"
+   tput sgr0
+   echo "See `pwd`/dm_real_sparse_elpa.log for details."
 else
    tput setaf 10
    echo " PASSED!"
@@ -185,6 +235,27 @@ else
    tput sgr0
 fi
 
+echo
+echo -n "Running the parallel 'elsi_dm_real_sparse + libOMM' Fortran test"
+${MPI_EXEC} -n 4 ./test_dm_real_sparse.x 2 ${ELSI_DIR}/test/H_real.csc ${ELSI_DIR}/test/S_real.csc 2 > dm_real_sparse_libomm.log &
+PID=$!
+while kill -0 $PID 2>/dev/null; do
+   sleep 1
+   echo -n '.'
+done
+
+if (! grep -q "Passed" <./dm_real_sparse_libomm.log); then
+   tput setaf 5
+   RED_ALART="true"
+   echo " FAILED!"
+   tput sgr0
+   echo "See `pwd`/dm_real_sparse_libomm.log for details."
+else
+   tput setaf 10
+   echo " PASSED!"
+   tput sgr0
+fi
+
 if [ "$DISABLE_CXX" != "yes" ]
 then
    echo
@@ -223,6 +294,27 @@ then
       echo " FAILED!"
       tput sgr0
       echo "See `pwd`/dm_complex_pexsi.log for details."
+   else
+      tput setaf 10
+      echo " PASSED!"
+      tput sgr0
+   fi
+
+   echo
+   echo -n "Running the parallel 'elsi_dm_real_sparse + PEXSI' Fortran test"
+   ${MPI_EXEC} -n 4 ./test_dm_real_sparse.x 3 ${ELSI_DIR}/test/H_real.csc ${ELSI_DIR}/test/S_real.csc 3 > dm_real_sparse_pexsi.log &
+   PID=$!
+   while kill -0 $PID 2>/dev/null; do
+      sleep 1
+      echo -n '.'
+   done
+
+   if (! grep -q "Passed" <./dm_real_sparse_pexsi.log); then
+      tput setaf 5
+      RED_ALART="true"
+      echo " FAILED!"
+      tput sgr0
+      echo "See `pwd`/dm_real_sparse_pexsi.log for details."
    else
       tput setaf 10
       echo " PASSED!"
