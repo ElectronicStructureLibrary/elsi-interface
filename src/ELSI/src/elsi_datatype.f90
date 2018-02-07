e608cdf7c347cf785ba4ff65c78632bdfcd61c64
Victor Yu <wy29@duke.edu>
2018-02-07 12:08:47 -0800

    VY: Fixes...

--- src/ELSI/src/elsi_datatype.f90
+++ src/ELSI/src/elsi_datatype.f90
@@ -242,7 +242,7 @@ module ELSI_DATATYPE
       integer(kind=i4) :: elpa_solver
       integer(kind=i4) :: elpa_n_single ! Number of single-precision steps
       logical          :: elpa_output
-      logical          :: elpa_started
+      logical          :: elpa_started = .false.
 
       ! libOMM
       integer(kind=i4) :: omm_n_states ! Number of states used in libOMM
@@ -267,7 +267,7 @@ module ELSI_DATATYPE
       integer(kind=i4) :: pexsi_comm_among_point
       integer(kind=i4) :: pexsi_comm_in_point
       real(kind=r8)    :: pexsi_ne ! Number of electrons computed by PEXSI
-      logical          :: pexsi_started
+      logical          :: pexsi_started = .false.
       integer(kind=c_intptr_t) :: pexsi_plan
       type(f_ppexsi_options)   :: pexsi_options
 
@@ -282,7 +282,7 @@ module ELSI_DATATYPE
       real(kind=r8)    :: chess_ev_ovlp_min
       real(kind=r8)    :: chess_ev_ovlp_max
       real(kind=r8)    :: chess_beta      ! Eigenspectrum estimate parameter
-      logical          :: chess_started
+      logical          :: chess_started = .false.
 
       ! SIPs
       integer(kind=i4) :: sips_n_elpa
@@ -294,7 +294,7 @@ module ELSI_DATATYPE
       real(kind=r8)    :: sips_interval(2)
       real(kind=r8)    :: sips_inertia_tol
       logical          :: sips_do_inertia
-      logical          :: sips_started
+      logical          :: sips_started = .false.
 
       ! DMP
       integer(kind=i4) :: dmp_n_states  ! Number of states used in DMP
