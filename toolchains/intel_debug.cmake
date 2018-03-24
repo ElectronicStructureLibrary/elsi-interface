78c5695dc1cf20a25e6b91e097366fefd0b53de7
Victor Yu <wy29@duke.edu>
2018-03-23 22:14:42 -0400

    CMake: Updated toolchain files.
    Now the same files can be used with "cmake -C" as well.

--- toolchains/intel_debug.cmake
+++ toolchains/intel_debug.cmake
@@ -1,12 +1,12 @@
 ### Generic Intel (debug) ###
 
-SET(CMAKE_Fortran_COMPILER "mpiifort")
-SET(CMAKE_C_COMPILER "mpiicc")
-SET(CMAKE_CXX_COMPILER "mpiicpc")
+SET(CMAKE_Fortran_COMPILER "mpiifort" CACHE STRING "MPI Fortran compiler")
+SET(CMAKE_C_COMPILER "mpiicc" CACHE STRING "MPI C compiler")
+SET(CMAKE_CXX_COMPILER "mpiicpc" CACHE STRING "MPI C++ compiler")
 
-SET(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -O3 -xAVX -fp-model precise -g -traceback -check bounds -check uninit -check pointers -fpe0" CACHE STRING "Fortran flags" FORCE)
-SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3 -xAVX -fp-model precise -std=c99" CACHE STRING "C flags" FORCE)
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -xAVX -fp-model precise -std=c++11" CACHE STRING "C++ flags" FORCE)
+SET(CMAKE_Fortran_FLAGS "-O3 -xAVX -fp-model precise -g -traceback -check bounds -check uninit -check pointers -fpe0" CACHE STRING "Fortran flags")
+SET(CMAKE_C_FLAGS "-O3 -xAVX -fp-model precise -std=c99" CACHE STRING "C flags")
+SET(CMAKE_CXX_FLAGS "-O3 -xAVX -fp-model precise -std=c++11" CACHE STRING "C++ flags")
 
 SET(ENABLE_TESTS "ON" CACHE BOOL "Enable Fortran tests")
 SET(ENABLE_C_TESTS "ON" CACHE BOOL "Enable C tests")
@@ -20,5 +20,5 @@ SET(OMM_INC "/home/wy29/opt/omm/build/include" CACHE PATH "libOMM include direct
 SET(SUPERLU_LIB "/home/wy29/opt/SuperLU_DIST_5.3.0/lib/libsuperlu_dist.a" CACHE STRING "SuperLU_DIST libraries")
 SET(SUPERLU_INC "/home/wy29/opt/SuperLU_DIST_5.3.0/SRC" CACHE PATH "SuperLU_DIST include directory")
 SET(PTSCOTCH_DIR "/home/wy29/opt/scotch_6.0.5a" CACHE PATH "PtScotch directory")
-SET(PTSCOTCH_LIB "${PTSCOTCH_DIR}/lib/libptscotchparmetis.a;${PTSCOTCH_DIR}/lib/libptscotch.a;${PTSCOTCH_DIR}/lib/libptscotcherr.a;${PTSCOTCH_DIR}/lib/libscotchmetis.a;${PTSCOTCH_DIR}/lib/libscotch.a;${PTSCOTCH_DIR}/lib/libscotcherr.a" CACHE STRING "PtScotch Libraries")
+SET(PTSCOTCH_LIB "${PTSCOTCH_DIR}/lib/libptscotchparmetis.a;${PTSCOTCH_DIR}/lib/libptscotch.a;${PTSCOTCH_DIR}/lib/libptscotcherr.a;${PTSCOTCH_DIR}/lib/libscotchmetis.a;${PTSCOTCH_DIR}/lib/libscotch.a;${PTSCOTCH_DIR}/lib/libscotcherr.a" CACHE STRING "PtScotch libraries")
 SET(PTSCOTCH_INC "${PTSCOTCH_DIR}/include" CACHE STRING "PtScotch include directory")
