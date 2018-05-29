812962d23ca3470014efd509291affa983ed383f
Victor Yu <wy29@duke.edu>
2018-05-29 08:07:37 -0700

    CMake: Fixed test/CMakeLists.txt.
    Also updated FortJSON which contains fixes for ifort 18.

--- external/FortJSON/src/fjson_constants.f90
+++ external/FortJSON/src/fjson_constants.f90
@@ -12,26 +12,26 @@ module FJSON_CONSTANTS
    implicit none
 
    !> Uninitialized value for integer(kind=i4) values
-   integer(kind=i4), parameter :: UNSET        = -910910_i4
+   integer(kind=i4), parameter :: UNSET = -910910_i4
 
    !> Uninitialized value for string values
-   character(len=*), parameter :: UNSET_STR    = "N/A"
+   character(len=*), parameter :: UNSET_STR = "N/A"
 
    !> Used by JSON scoping stack to denote the element is not set
-   integer(kind=i4), parameter :: JSON_NULL    = 0
+   integer(kind=i4), parameter :: JSON_NULL = 0
 
    !> Used by JSON scoping stack to denote the element is the root/top-level of
    !! the JSON file
-   integer(kind=i4), parameter :: JSON_ROOT    = 1
+   integer(kind=i4), parameter :: JSON_ROOT = 1
 
    !> Used by JSON scoping stack to denote the element is an object
-   integer(kind=i4), parameter :: JSON_OBJECT  = 2
+   integer(kind=i4), parameter :: JSON_OBJECT = 2
 
    !> Used by JSON scoping stack to denote the element is an array
-   integer(kind=i4), parameter :: JSON_ARRAY   = 3
+   integer(kind=i4), parameter :: JSON_ARRAY = 3
 
    !> Used by error handling to denote no error has been encountered
-   integer(kind=i4), parameter :: NO_ERROR     = 0
+   integer(kind=i4), parameter :: NO_ERROR = 0
 
    !> Used by error handling to denote that an error occured when opening a file
    integer(kind=i4), parameter :: FILE_OPEN_ERROR = 1
@@ -65,6 +65,6 @@ module FJSON_CONSTANTS
    integer(kind=i4), parameter :: FILE_WRITE_ERROR = 8
 
    !> Number of characters in RFC3339 datetime string with time zone offset.
-   integer(kind=i4), parameter :: DATETIME_LEN     = 29
+   integer(kind=i4), parameter :: DATETIME_LEN = 29
 
 end module FJSON_CONSTANTS
