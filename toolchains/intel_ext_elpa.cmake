17b93fbad1681a87246797c26b35c132110e8684
William Huhn <william.huhn@duke.edu>
2018-08-01 19:10:38 -0700

    Add toolchain file for external ELPA

---
+++ toolchains/intel_ext_elpa.cmake
@@ -0,0 +1,19 @@
+### Generic Intel ###
+
+SET(CMAKE_Fortran_COMPILER "mpiifort" CACHE STRING "MPI Fortran compiler")
+SET(CMAKE_C_COMPILER "mpiicc" CACHE STRING "MPI C compiler")
+SET(CMAKE_CXX_COMPILER "mpiicpc" CACHE STRING "MPI C++ compiler")
+
+SET(CMAKE_Fortran_FLAGS "-O3 -xAVX -fp-model precise" CACHE STRING "Fortran flags")
+SET(CMAKE_C_FLAGS "-O3 -xAVX -fp-model precise" CACHE STRING "C flags")
+SET(CMAKE_CXX_FLAGS "-O3 -xAVX -fp-model precise" CACHE STRING "C++ flags")
+
+SET(ENABLE_TESTS ON CACHE BOOL "Enable Fortran tests")
+SET(ENABLE_C_TESTS ON CACHE BOOL "Enable C tests")
+SET(ENABLE_PEXSI ON CACHE BOOL "Enable PEXSI")
+SET(ELPA2_KERNEL "AVX" CACHE STRING "Use ELPA AVX kernel")
+SET(USE_EXTERNAL_ELPA ON CACHE BOOL "Use external ELPA")
+
+SET(INC_PATHS "$ENV{ELPAROOT}/include/elpa-2018.05.001/modules" CACHE STRING "Include paths")
+SET(LIB_PATHS "$ENV{MKLROOT}/lib/intel64 $ENV{ELPAROOT}/lib" CACHE STRING "External library paths")
+SET(LIBS "mkl_scalapack_lp64 mkl_blacs_intelmpi_lp64 mkl_intel_lp64 mkl_sequential mkl_core elpa" CACHE STRING "External libraries")
