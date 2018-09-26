329ee96b3a4e48c54f1f19bcace603fd13a29985
Victor Yu <wy29@duke.edu>
2018-09-26 10:32:49 -0400

    NTPoly: Remove .F90 files.

--- external/NTPoly/src/NTMPIModule.F90
+++
@@ -1,6 +0,0 @@
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!> This module wraps the MPI include statement because on certain platforms
-!! just writing "USE MPI" does not work.
-MODULE NTMPIModule
-  include "mpif.h"
-END MODULE
