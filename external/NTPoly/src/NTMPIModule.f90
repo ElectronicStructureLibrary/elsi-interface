e9247b8e33b4e02fec61cb364d58988e342ac904
Victor Yu <wy29@duke.edu>
2018-09-26 10:33:48 -0400

    NTPoly: Add NTMPIModule.f90.

---
+++ external/NTPoly/src/NTMPIModule.f90
@@ -0,0 +1,6 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!> This module wraps the MPI include statement because on certain platforms
+!! just writing "USE MPI" does not work.
+MODULE NTMPIModule
+  include "mpif.h"
+END MODULE
