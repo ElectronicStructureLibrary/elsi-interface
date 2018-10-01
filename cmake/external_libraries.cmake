4765e1285aa0d9e51fc41b36a8d71ca4857cadb1
Victor Yu <wy29@duke.edu>
2018-10-01 16:13:06 -0400

    CMake: Support Fortran-only build without C.

--- cmake/external_libraries.cmake
+++ cmake/external_libraries.cmake
@@ -9,21 +9,21 @@ MACRO(convert_to_list var)
   ENDIF()
 ENDMACRO()
 
-# Go through directories listed in LIB_PATHS and turn entries of LIBS into targets.
+# Go through directories listed in LIB_PATHS and turn entries of LIBS into actual targets.
 FUNCTION(generate_library_targets _PATHS _LIBRARIES)
   FOREACH(LIB ${${_LIBRARIES}})
-    FIND_LIBRARY(LIB_FULLPATH ${LIB} HINTS ${${_PATHS}})
-
-    IF(LIB_FULLPATH)
-      MESSAGE(STATUS "Found ${LIB_FULLPATH}")
-      IF(NOT TARGET ${LIB})
+    # If the target already exists, skip it.
+    # This can occur when the same library is set multiple times in LIBS due to cyclic dependencies.
+    IF(NOT TARGET ${LIB})
+      FIND_LIBRARY(LIB_FULLPATH ${LIB} HINTS ${${_PATHS}})
+      IF(LIB_FULLPATH)
+        MESSAGE(STATUS "Found ${LIB_FULLPATH}")
         ADD_LIBRARY(${LIB} UNKNOWN IMPORTED)
+        SET_TARGET_PROPERTIES(${LIB} PROPERTIES IMPORTED_LOCATION ${LIB_FULLPATH})
+        UNSET(LIB_FULLPATH CACHE)
+      ELSE()
+        MESSAGE(FATAL_ERROR "${Magenta}Could not find ${LIB}${ColorReset}")
       ENDIF()
-      SET_TARGET_PROPERTIES(${LIB} PROPERTIES IMPORTED_LOCATION ${LIB_FULLPATH})
-    ELSE()
-      MESSAGE(FATAL_ERROR "${Magenta}Could not find ${LIB}${ColorReset}")
     ENDIF()
-
-    UNSET(LIB_FULLPATH CACHE)
   ENDFOREACH()
 ENDFUNCTION()
