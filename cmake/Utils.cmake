227a7b1cd16e6d8ad7e39328181426db361ea839
Victor Yu <wy29@duke.edu>
2018-10-22 13:37:56 -0400

    CMake: Include color message at the beginning.
    In case of errors, this gives the error message a custom color.

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,35 +1,4 @@
-# Taken from FHI-aims with permission of copyright holders
-
-# Convert a string into a list
-# Do nothing if input is already a list
-MACRO(convert_to_list var)
-  LIST(LENGTH ${var} _length)
-  IF(_length EQUAL 1)
-    STRING(REPLACE " " ";" ${var} ${${var}})
-  ENDIF()
-ENDMACRO()
-
-# Go through directories listed in LIB_PATHS and turn entries of LIBS into actual targets
-FUNCTION(generate_library_targets _PATHS _LIBRARIES)
-  FOREACH(LIB ${${_LIBRARIES}})
-    # If the target already exists, skip it.
-    # This can occur when the same library is set multiple times in LIBS due to cyclic dependencies
-    IF(NOT TARGET ${LIB})
-      FIND_LIBRARY(LIB_FULLPATH ${LIB} HINTS ${${_PATHS}})
-      IF(LIB_FULLPATH)
-        MESSAGE(STATUS "Found ${LIB_FULLPATH}")
-        ADD_LIBRARY(${LIB} UNKNOWN IMPORTED)
-        SET_TARGET_PROPERTIES(${LIB} PROPERTIES IMPORTED_LOCATION ${LIB_FULLPATH})
-        UNSET(LIB_FULLPATH CACHE)
-      ELSE()
-        MESSAGE(FATAL_ERROR "${Magenta}Could not find ${LIB}${ColorReset}")
-      ENDIF()
-    ENDIF()
-  ENDFOREACH()
-ENDFUNCTION()
-
 # Taken from http://stackoverflow.com/a/19578320
-
 STRING(ASCII 27 Esc)
 
 SET(COLORRESET "${Esc}[m")
@@ -48,3 +17,33 @@ SET(BOLDBLUE "${Esc}[1;34m")
 SET(BOLDMAGENTA "${Esc}[1;35m")
 SET(BOLDCYAN "${Esc}[1;36m")
 SET(BOLDWHITE "${Esc}[1;37m")
+
+# Taken from FHI-aims with permission of copyright holders
+# Convert a string into a list
+# Do nothing if input is already a list
+MACRO(convert_to_list var)
+  LIST(LENGTH ${var} _length)
+  IF(_length EQUAL 1)
+    STRING(REPLACE " " ";" ${var} ${${var}})
+  ENDIF()
+ENDMACRO()
+
+# Taken from FHI-aims with permission of copyright holders
+# Go through directories listed in LIB_PATHS and turn entries of LIBS into actual targets
+FUNCTION(generate_library_targets _PATHS _LIBRARIES)
+  FOREACH(LIB ${${_LIBRARIES}})
+    # If the target already exists, skip it.
+    # This can occur when the same library is set multiple times in LIBS due to cyclic dependencies
+    IF(NOT TARGET ${LIB})
+      FIND_LIBRARY(LIB_FULLPATH ${LIB} HINTS ${${_PATHS}})
+      IF(LIB_FULLPATH)
+        MESSAGE(STATUS "Found ${LIB_FULLPATH}")
+        ADD_LIBRARY(${LIB} UNKNOWN IMPORTED)
+        SET_TARGET_PROPERTIES(${LIB} PROPERTIES IMPORTED_LOCATION ${LIB_FULLPATH})
+        UNSET(LIB_FULLPATH CACHE)
+      ELSE()
+        MESSAGE(FATAL_ERROR "${Magenta}Could not find ${LIB}${ColorReset}")
+      ENDIF()
+    ENDIF()
+  ENDFOREACH()
+ENDFUNCTION()
