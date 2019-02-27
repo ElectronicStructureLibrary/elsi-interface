f59c7af967575848e038bbe3fff6282ef6d7ac08
Victor Yu <wy29@duke.edu>
2019-02-27 17:52:37 -0500

    Update documentation for SuperLU_DIST 6.1.1

--- toolchains/intel_ext_superlu.cmake
+++ toolchains/intel_ext_superlu.cmake
@@ -12,6 +12,6 @@ SET(ENABLE_PEXSI ON CACHE BOOL "Enable PEXSI")
 SET(USE_EXTERNAL_SUPERLU ON CACHE BOOL "Use external SuperLU_DIST")
 SET(ENABLE_TESTS ON CACHE BOOL "Enable Fortran tests")
 
-SET(INC_PATHS "/home/wy29/opt/SuperLU_DIST_6.1.0/SRC /home/wy29/opt/scotch_6.0.0/include" CACHE STRING "External library include paths")
-SET(LIB_PATHS "$ENV{MKLROOT}/lib/intel64 /home/wy29/opt/SuperLU_DIST_6.1.0/lib /home/wy29/opt/scotch_6.0.0/lib" CACHE STRING "External library paths")
+SET(INC_PATHS "/home/wy29/opt/SuperLU_DIST_6.1.1/SRC /home/wy29/opt/scotch_6.0.0/include" CACHE STRING "External library include paths")
+SET(LIB_PATHS "$ENV{MKLROOT}/lib/intel64 /home/wy29/opt/SuperLU_DIST_6.1.1/lib /home/wy29/opt/scotch_6.0.0/lib" CACHE STRING "External library paths")
 SET(LIBS "superlu_dist ptscotchparmetis ptscotch ptscotcherr scotchmetis scotch scotcherr mkl_scalapack_lp64 mkl_blacs_intelmpi_lp64 mkl_intel_lp64 mkl_sequential mkl_core" CACHE STRING "External libraries")
