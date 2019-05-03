8f9e203f37347d6f60f05f2de931e406a527648e
Victor Yu <wy29@duke.edu>
2019-05-03 15:45:03 -0400

    Add DM/EDM computation to public API

--- src/elsi_interface.f90
+++ src/elsi_interface.f90
@@ -17,6 +17,7 @@ module ELSI
    use ELSI_RW
    use ELSI_SETUP
    use ELSI_SOLVER
+   use ELSI_UTIL
 
    implicit none
 
@@ -139,6 +140,10 @@ module ELSI
    public :: elsi_extrapolate_dm_complex_sparse
    public :: elsi_compute_mu_and_occ
    public :: elsi_compute_entropy
+   public :: elsi_compute_dm_real
+   public :: elsi_compute_dm_complex
+   public :: elsi_compute_edm_real
+   public :: elsi_compute_edm_complex
 
    ! Reading and writing matrix
    public :: elsi_init_rw
