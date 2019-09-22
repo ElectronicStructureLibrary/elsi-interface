cb0c6878b6321f76bbf5242a45ce5267efa13b5a
Victor Yu <wy29@duke.edu>
2019-09-21 23:45:40 -0400

    Update CMake toolchain files
    [skip ci]

--- toolchains/summit.cmake
+++ toolchains/summit.cmake
@@ -1,22 +1,23 @@
-### Summitdev ###
+### Summit ###
 
-# module load cmake/3.13.4
-# module load essl/6.1.0-1
+# module load cmake/3.15.2
+# module load xl/16.1.1-3
+# module load spectrum-mpi/10.3.0.0-20190419
+# module load essl/6.2.0-20190419
 # module load netlib-lapack/3.8.0
 # module load netlib-scalapack/2.0.2
-# module load xl/16.1.1-beta6
 
 SET(CMAKE_Fortran_COMPILER "mpixlf" CACHE STRING "MPI Fortran compiler")
 SET(CMAKE_C_COMPILER "mpixlc" CACHE STRING "MPI C compiler")
 SET(CMAKE_CXX_COMPILER "mpixlC" CACHE STRING "MPI C++ compiler")
 
-SET(CMAKE_Fortran_FLAGS "-Ofast -qarch=pwr8 -qstrict -qxlf2003=polymorphic" CACHE STRING "Fortran flags")
-SET(CMAKE_C_FLAGS "-Ofast -qarch=pwr8 -qstrict -qlanglvl=stdc99" CACHE STRING "C flags")
-SET(CMAKE_CXX_FLAGS "-Ofast -qarch=pwr8 -qstrict -qlanglvl=extended0x" CACHE STRING "C++ flags")
+SET(CMAKE_Fortran_FLAGS "-Ofast -qarch=pwr9 -qstrict" CACHE STRING "Fortran flags")
+SET(CMAKE_C_FLAGS "-Ofast -qarch=pwr9 -qstrict -qlanglvl=stdc99" CACHE STRING "C flags")
+SET(CMAKE_CXX_FLAGS "-Ofast -qarch=pwr9 -qstrict -qlanglvl=extended0x" CACHE STRING "C++ flags")
 
 SET(ENABLE_PEXSI ON CACHE BOOL "Enable PEXSI")
 SET(ENABLE_TESTS ON CACHE BOOL "Enable tests")
 SET(ADD_UNDERSCORE OFF CACHE BOOL "Do not suffix C functions with an underscore")
 
-SET(LIB_PATHS "/sw/summitdev/essl/5.5.0-20161110/lib64" CACHE STRING "External library paths")
+SET(LIB_PATHS "$ENV{OLCF_ESSL_ROOT}/lib64" CACHE STRING "External library paths")
 SET(LIBS "essl;scalapack;lapack;blas" CACHE STRING "External libraries")
