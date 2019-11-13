de5601c9a1ec5a47f72567c5f039ada535aa5811
Victor Yu <wy29@duke.edu>
2019-11-12 20:22:50 -0500

    Update SuperLU_DIST to v6.2.0

--- external/SuperLU_DIST/src/superlu_dist_config.h
+++ external/SuperLU_DIST/src/superlu_dist_config.h
@@ -1,2 +1,10 @@
-#define HAVE_PARMETIS TRUE
+/* superlu_dist_config.h.in */
+
 #define XSDK_INDEX_SIZE 32
+#define HAVE_PARMETIS TRUE
+#define SLU_HAVE_LAPACK TRUE
+
+#if (XSDK_INDEX_SIZE == 64)
+#define _LONGINT 1
+
+#endif
