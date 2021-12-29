f612c86e061725f26a25a9b8c2d2e1358f5e6103
Ruyi Song <rs416@timewarp.egr.duke.edu>
2021-12-29 00:16:10 -0500

    try to use mannually count nnz_l

--- src/elsi_omm.f90
+++ src/elsi_omm.f90
@@ -107,10 +107,22 @@ subroutine elsi_solve_omm_real(ph,bh,ham,ovlp,coeff,dm)
 
    character(len=*), parameter :: caller = "elsi_solve_omm_real"
 
+   integer(kind=i8) i,j
+
    ! Compute sparsity
    if(bh%nnz_g == UNSET) then
       if(bh%nnz_l == UNSET) then
-         bh%nnz_l = count( (abs(ham) > bh%def0), kind=c_int64_t)
+!RS: The following count kind specification cannot work for nvfortran compiler
+!    neither i8 nor 8 works
+!
+!         bh%nnz_l = count( (abs(ham) > bh%def0), kind=i8)
+         bh%nnz_l = 0
+         do i = 1, bh%n_lrow
+            do j = 1, bh%n_lcol
+               if (abs(ham(i,j)) > bh%def0) bh%nnz_l=bh%nnz_l+1 
+            enddo
+         enddo
+
       end if
 
       call MPI_Allreduce(bh%nnz_l,bh%nnz_g,1,MPI_INTEGER8,MPI_SUM,bh%comm,ierr)
